466a128eb29d8272d30424aaa82c28daae81d232
alamgonza3-png <alamgonza3@gmail.com>
2025-09-02 10:59:41 -0300

    punto 6

---
+++ main.py
@@ -0,0 +1,4 @@
+"""6- De la siguiente cadena “La lluvia en Mendoza es escasa” indique cual es el tamaño
+de la cadena es decir su número de caracteres."""
+longitud = len ("La lluvia en Mendoza es escasa ")
+print  ("la lluvia en Mendoza es escasa tiene" , longitud , "caracteres" )
