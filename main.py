856fcdef5f2f9c4454a3a9be03db9bc2ba22738b
francosampieri <francosampiee@gmail.com>
2025-09-03 00:23:26 -0300

    arreglar errores

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-"""- Si se asigna un valor a una variable fuera de rango (mayor de lo establecido) ¿Qué 
-ocurre? ¿Existe alguna forma de resolverlo? Ejemplifique. """
-"""EJERCICIO 2"""
-#La mejor forma de pedir un valor dentro de un rango es utilizar un bucle while que se repita hasta que el usuario ingrese un valor correcto.
-numero_usuario = int(input("Ingresa un numero entre 2 y 10"))
-while numero_usuario > 10 or numero_usuario < 2: 
-    numero_usuario = int(input("ERROR: Ingresa un numero entre 2 y 10"))
+#Ejercicio 19: #
+
+class Operacion_matematicas:
+    def __init__(self,valor1,valor2):
+        self.valor1=valor1
+        self.valor2=valor2
+
+    
+    def sumarNumeros(self):
+       suma= self.valor1+self.valor2
+       print(f"La suma de {self.valor1} y {self.valor2} es {suma}")
+    
+    
+    def restarNumeros(self):
+        resta= self.valor1-self.valor2
+        print(f"La resta de {self.valor1} y {self.valor2} es {resta}")
+        
+    def multiplicarNumeros(self):
+        multiplicacion= self.valor1*self.valor2
+        print(f"La multiplicacion de {self.valor1} y {self.valor2} es {multiplicacion}")
+
+    def dividirNumeros(self):
+        division= self.valor1/self.valor2
+        print(f"La division de {self.valor1} y {self.valor2} es {division}")
+
+    def aplicarOperaciones(self, operacion):
+        if operacion=="+":
+            self.sumarNumeros()
+        elif operacion=="-":
+            self.restarNumeros()
+        elif operacion=="*":
+            self.multiplicarNumeros()
+        elif operacion=="/":
+            self.dividirNumeros()
+        
+class Calculo: 
+    def main(self):
+        operacion = Operacion_matematicas(10,5)
+        operacion.aplicarOperaciones("+")
+        operacion.aplicarOperaciones("-")
+        operacion.aplicarOperaciones("*")
+        operacion.aplicarOperaciones("/")
+
+laconchadetumadre = Calculo()
+laconchadetumadre.main()
